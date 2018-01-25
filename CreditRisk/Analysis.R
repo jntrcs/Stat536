1ccecd4357a91eb16c75ac73a0c2804ce00aa675
Jackson Curtis <jcurtis210@gmail.com>
2018-01-25 13:47:50 -0700

    .

--- CreditRisk/Analysis.R
+++ CreditRisk/Analysis.R
@@ -13,7 +13,6 @@ pdf(file="AVPlot.pdf")
 avPlots(mod, terms=~.-Ethnicity-Student-Gender-Married)
 dev.off()
 
-mod<-lm()
 
 require(GGally)
 ggpairs(data)
@@ -40,8 +39,26 @@ pnorm(-2)*2*294 #expected number of residuals greater than +-2
 data$leverage<-influence(mod)$hat
 plot(data$leverage~data$Limit)
 data$cooks<-cooks.distance(mod)
+data$fitted = fitted(mod)
 plot(data$cooks~data$Income)
 mean(data$leverage)*2
 data$predictions<-fitted(mod)
 plot(data$residual~data$predictions)
 plot(mod)
+
+require(ggplot2)
+pdf(file="resids.pdf")
+ggplot(data)+geom_histogram(aes(x=residual), binwidth = .4)+xlab("Residuals")+ylab("Count")+
+  theme(axis.text.x=element_text(size=18, angle=0, vjust=0.3), axis.title.x = element_text(size=22),
+        axis.title.y=element_text(size=22),
+        axis.text.y=element_text(size=16),
+        plot.title=element_text(size=16))
+dev.off()
+
+pdf(file="fitted.pdf")
+ggplot(data)+geom_point(aes(y=residual, x=fitted))+ylab("Standardized Residuals")+xlab("Fitted Values")+
+  theme(axis.text.x=element_text(size=18, angle=0, vjust=0.3), axis.title.x = element_text(size=22),
+        axis.title.y=element_text(size=22),
+        axis.text.y=element_text(size=16),
+        plot.title=element_text(size=16))
+dev.off()
